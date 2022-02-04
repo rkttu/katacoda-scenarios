629b49bb58584c91f419e1c7419e7ec79243c5a0
Jung Hyun Nam <rkttu@rkttu.com>
2022-02-04 17:50:34 +0900

    apt install 후 멈추지 않도록 수정

--- powershell/foreground.sh
+++ powershell/foreground.sh
@@ -3,8 +3,10 @@
 # Update the list of packages
 sudo apt-get update
 
+# Note: https://askubuntu.com/questions/372810/how-to-prevent-script-not-to-stop-after-apt-get
+
 # Install pre-requisite packages.
-sudo apt-get install -y wget apt-transport-https software-properties-common
+sudo apt-get install -y wget apt-transport-https software-properties-common < "/dev/null"
 
 # Download the Microsoft repository GPG keys
 wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
@@ -16,7 +18,7 @@ sudo dpkg -i /tmp/packages-microsoft-prod.deb
 sudo apt-get update
 
 # Install PowerShell
-sudo apt-get install -y powershell
+sudo apt-get install -y powershell < "/dev/null"
 
 # Start PowerShell
 pwsh
