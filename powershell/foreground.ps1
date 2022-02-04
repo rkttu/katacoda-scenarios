21d8cd237f695eb1778d03c4d4cc52118e7725af
Jung Hyun Nam <rkttu@rkttu.com>
2022-02-04 15:01:02 +0900

    실험용 PowerShell 모듈 추가

---
+++ powershell/foreground.ps1
@@ -0,0 +1,7 @@
+#!/usr/bin/env pwsh
+
+Write-Host 'This is a bash shell script.\n\n'
+Write-Host 'Any commands you put in here will be executed at runtime.\n\n'
+Write-Host 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
+Write-Host 'The file is named foreground.sh because when it executes, the commands are echoed to the terminal.\n\n'
+Write-Host 'To run a script in the background, so learners will not see it execute, specify it in the index.json as "background": "background.sh"\n\n'
