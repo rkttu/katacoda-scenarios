0727a55d1873f1e04df10bddd558318e994e0898
Jung Hyun Nam <rkttu@rkttu.com>
2022-02-04 17:46:59 +0900

    PowerShell 샘플 예제 변경

--- powershell/foreground.sh
+++ powershell/foreground.sh
@@ -7,10 +7,10 @@ sudo apt-get update
 sudo apt-get install -y wget apt-transport-https software-properties-common
 
 # Download the Microsoft repository GPG keys
-wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
 
 # Register the Microsoft repository GPG keys
-sudo dpkg -i packages-microsoft-prod.deb
+sudo dpkg -i /tmp/packages-microsoft-prod.deb
 
 # Update the list of packages after we added packages.microsoft.com
 sudo apt-get update
